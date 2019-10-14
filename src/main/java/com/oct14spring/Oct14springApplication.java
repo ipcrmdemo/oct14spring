c3cac626829cbca4a9dce45dd8116b4b00903119
Matt Cadorette <mcadorette@gmail.com>
2019-10-14 15:26:32 -0400

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/oct14spring/Oct14springApplication.java
@@ -0,0 +1,12 @@
+package com.oct14spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oct14springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oct14springApplication.class, args);
+	}
+}
---
+++ src/test/java/com/oct14spring/Oct14springApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oct14spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Oct14springApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
